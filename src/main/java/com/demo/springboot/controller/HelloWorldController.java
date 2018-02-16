8bd1a53901bf538d2fd68fe1ddca7ff5dd3c1a6b
nandakumarpurohit <nandakumarpurohit@gmail.com>
2018-02-15 20:33:29 -0500

    init files

---
+++ src/main/java/com/demo/springboot/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.demo.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+   @RequestMapping("/")
+   public String sayHello() {
+      return "Hello Spring Boot!!";
+   }
+}
